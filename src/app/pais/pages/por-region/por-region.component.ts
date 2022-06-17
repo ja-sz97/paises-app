64f532bbe42742b38eaf8507e1cdb7ad310ae006
javier <javier.saavedraz@utem.cl>
2022-06-17 18:43:35 -0400

    avance pais rest app

---
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-por-region',
+  templateUrl: './por-region.component.html',
+  styles: [`
+    button{
+      margin-rigth: 5px;
+    }
+  `
+  ]
+})
+export class PorRegionComponent  {
+
+  regiones: string [] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+
+  constructor() {}
+  
+  activarRegion( region: string ){
+    this.regionActiva = region;
+  }
+
+
+}
